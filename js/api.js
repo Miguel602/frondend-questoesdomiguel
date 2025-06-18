a023b288b389382d896c33847dce15b136c52d8d
Miguel602 <99503433+Miguel602@users.noreply.github.com>
2025-06-18 09:32:22 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,2 +1,27 @@
 // URL do backend - altere para a URL do seu Render
 const API_BASE_URL = 'https://questoes-do-miguel.onrender.com';
+import { User } from "./models/User.js";
+import bcrypt from "bcrypt";
+
+app.get("/create-admin", async (req, res) => {
+  try {
+    const username = "admin";
+    const password = "admin123";
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const [user, created] = await User.findOrCreate({
+      where: { username },
+      defaults: { password: hashedPassword }
+    });
+
+    if (created) {
+      res.send("Usuário admin criado com sucesso!");
+    } else {
+      res.send("Usuário admin já existe.");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erro ao criar admin.");
+  }
+});
+
